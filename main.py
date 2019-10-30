d9ee193387062f056ec8d9fd41f570b97448acbf
DragonPyli <ezio@MSI.localdomain>
2019-10-30 18:26:01 +0530

    Changed From Process To Threading

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import time
+
 try :
     import threading
+    from threading import Thread
 except ImportError as error :
     print(error)
     print("Please Try Again After Installing 'threading'")
@@ -16,18 +19,18 @@ except ImportError as error :
     print(error)
     print("Please Try Again After Installing Required Packages")
 
-def MainLoopConsole(cVar,lock) :
+def MainLoopConsole(cVar) :
+    time.sleep(1)
     print("[+] Main Loop Console Process Started")
     x = " "
     while(x.lower()!='exit') :
-        lock.acquire()
         try:
             x = input(">>> ")
         except :
             print("[!] InputError")
         print("[Echo] :",x)
-    lock.release()
     cVar['exit'] = 1 
+    print("[*] Exit MainLoop Handle !") 
 
 def BackGroundHandle(cVar) :
     print("[+] BackGround Handle Process Started")
@@ -38,26 +41,25 @@ def BackGroundHandle(cVar) :
     print("[*] Exit Background Handle !")
 
 def main() :
-    manager = Manager()
-    cVar = manager.dict()
+    cVar = {}
     cVar['exit'] = 0 
     cVar['errors'] = [0,0,0,0,0]
     cVar['entered'] = 0
-
-    lock = Lock()
     
-    mainLoopProcess = Process(target=MainLoopConsole,args=(cVar,lock))
-    bgProcess = Process(target=BackGroundHandle,args=(cVar,))
+    mainLoopThread = Thread(target=MainLoopConsole,args=(cVar,))
+    bgThread = Thread(target=BackGroundHandle,args=(cVar,))
 
-    Procs = [mainLoopProcess,bgProcess]
+    global Procs
+    Procs = [mainLoopThread,bgThread]
 
     for p in Procs :
         p.start()
         print("[+] Start",p)
-    for p in Procs :
-        p.join()
-        print("[+] Join",p)
     
 
 if __name__ == "__main__" :
     main()
+    for p in Procs :
+        while p.isAlive() :
+            pass
+        print("[+] Stopped",p) 
